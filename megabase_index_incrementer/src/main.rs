f6ac54d1f533d26c30ffe09d77f5f54c4dd6983a
mulark <mulark11@gmail.com>
2020-07-22 21:52:40 -0600

    Took way too long but added author field to metadata

--- megabase_index_incrementer/src/main.rs
+++ megabase_index_incrementer/src/main.rs
@@ -26,6 +26,7 @@ struct Megabases {
 struct MegabaseMetadata {
     /// The filename of this megabase.
     name: String,
+    author: Option<String>,
     /// The post/video showcasing the megabase.
     source_link: String,
     /// The version of Factorio this save was saved with.
@@ -104,11 +105,12 @@ impl PartialOrd for FactorioVersion {
 
 fn main() -> io::Result<()> {
     loop {
+        upgrade_existing_metadatas().unwrap();
         let mut metadata = MegabaseMetadata::default();
         let mut s = String::new();
         println!("Enter a base source link for the post describing the megabase.");
         stdin().read_line(&mut s)?;
-        check_url_alive(&s)?;
+        metadata.author = Some(check_url_alive_get_author(&s).unwrap());
         s = s.trim().to_string();
         metadata.source_link = s.clone();
         s.clear();
@@ -141,23 +143,65 @@ fn main() -> io::Result<()> {
             metadata.factorio_version.to_string(),
             metadata.sha256
         );
-        let megabases_json = PathBuf::from("megabases.json");
-        if megabases_json.exists() {
-            let s = std::fs::read_to_string(&megabases_json)?;
-            let mut megabases = serde_json::from_str::<Megabases>(&s)?;
-            megabases.saves.push(metadata);
-            megabases.saves.sort();
-            megabases.saves.dedup();
-            let s = serde_json::to_string_pretty(&megabases)?;
-            write!(std::fs::File::create(&megabases_json)?, "{}", s)?;
-        } else {
-            let megabases = Megabases {
-                saves: vec![metadata],
-            };
-            let s = serde_json::to_string_pretty(&megabases)?;
-            write!(std::fs::File::create(megabases_json)?, "{}", s)?;
+        write_metadata_to_disk(metadata).unwrap();
+    }
+}
+
+fn write_metadata_to_disk(metadata: MegabaseMetadata) -> Result<(), Box<dyn std::error::Error>> {
+    let megabases_json = PathBuf::from("megabases.json");
+    if megabases_json.exists() {
+        let s = std::fs::read_to_string(&megabases_json)?;
+        let mut megabases = serde_json::from_str::<Megabases>(&s)?;
+        megabases.saves.push(metadata);
+        megabases.saves.sort();
+        megabases.saves.dedup();
+        let s = serde_json::to_string_pretty(&megabases)?;
+        write!(std::fs::File::create(&megabases_json)?, "{}", s)?;
+    } else {
+        let megabases = Megabases {
+            saves: vec![metadata],
+        };
+        let s = serde_json::to_string_pretty(&megabases)?;
+        write!(std::fs::File::create(megabases_json)?, "{}", s)?;
+    }
+    Ok(())
+}
+
+/// Removes an existing metadata from the json where the sha256sum matches the provided sha256.
+fn remove_metadata_from_disk_with_sha256(sha256: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let megabases_json = PathBuf::from("megabases.json");
+    if megabases_json.exists() {
+        let s = std::fs::read_to_string(&megabases_json)?;
+        let mut megabases = serde_json::from_str::<Megabases>(&s)?;
+        megabases.saves.retain(|metadata| metadata.sha256 != sha256);
+        let s = serde_json::to_string_pretty(&megabases)?;
+        write!(std::fs::File::create(&megabases_json)?, "{}", s)?;
+    }
+    Ok(())
+}
+
+fn upgrade_existing_metadatas() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Checking to upgrade existing metadata");
+    let megabases_json = PathBuf::from("megabases.json");
+    let s = std::fs::read_to_string(&megabases_json)?;
+    let megabases = serde_json::from_str::<Megabases>(&s)?;
+    for mut metadata in megabases.saves {
+        if metadata.author.is_none() {
+            println!("Upgrading {}", metadata.name);
+            println!("{}", metadata.source_link);
+            metadata.author = Some(check_url_alive_get_author(&metadata.source_link).unwrap());
+            remove_metadata_from_disk_with_sha256(&metadata.sha256)?;
+            write_metadata_to_disk(metadata.clone())?;
+        }
+        if let Some(author) = &metadata.author {
+            if author.starts_with("/user/") {
+                metadata.author = Some("https://www.reddit.com".to_owned() + author);
+                remove_metadata_from_disk_with_sha256(&metadata.sha256)?;
+                write_metadata_to_disk(metadata.clone())?;
+            }
         }
     }
+    Ok(())
 }
 
 pub fn sha256sum(file_path: &PathBuf) -> String {
@@ -266,12 +310,55 @@ fn factorio_exe() -> PathBuf {
 }
 
 /// Tests if a url is available
-fn check_url_alive(url: &str) -> io::Result<()> {
-    let resp = ureq::head(url).call();
-    if resp.status() == 200 {
-        Ok(())
+fn check_url_alive_get_author(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+    if url.contains("reddit.com") {
+        let r = ureq::get(&url).call().into_string()?;
+        if r.contains("\"author\":") {
+            let re = regex::Regex::new("\"author\":\"([^\"]+)\"").unwrap();
+            let matches = re.captures_iter(&r);
+            for cap in matches {
+                let cap = format!("/user/{}/", &cap[1]);
+                println!("Found post to be submitted by {}, press enter if this is correct, \"n\" to see next user"
+                    , cap);
+                let mut s = String::new();
+                stdin().read_line(&mut s)?;
+                s = s.trim().to_owned();
+                if s.is_empty() {
+                    return Ok(cap);
+                }
+            }
+            panic!("Didn't find any user");
+        } else {
+            let re = regex::Regex::new("/u/([^/\"\x20]+)[/\"\x20]").unwrap();
+            let matches = re.captures_iter(&r);
+            for cap in matches {
+                let cap = format!("/user/{}/", &cap[1]);
+                println!("Found post to be submitted by {}, press enter if this is correct, \"n\" to see next user"
+                    , cap);
+                let mut s = String::new();
+                stdin().read_line(&mut s)?;
+                s = s.trim().to_owned();
+                if s.is_empty() {
+                    return Ok(cap);
+                }
+            }
+            panic!("Didn't find any user");
+        }
     } else {
-        Err(io::Error::new(io::ErrorKind::Other, format!("{:?}", resp)))
+        let r = ureq::head(url).call();
+        if r.status() == 200 {
+            println!("Please enter the user who created the save");
+            let mut s = String::new();
+            stdin().read_line(&mut s)?;
+            s = s.trim().to_owned();
+            if !s.is_empty() {
+                Ok(s)
+            } else {
+                panic!("Author is required");
+            }
+        } else {
+            panic!("Status code error for URL {}", url);
+        }
     }
 }
 
@@ -309,6 +396,7 @@ mod tests {
         let metadata =
         MegabaseMetadata {
             name: "0.17-0.18 Poobers Beautiful Base.zip".to_owned(),
+            author: None,
             source_link: "https://www.youtube.com/watch?v=hMrxuaIdeeE".to_owned(),
             factorio_version: FactorioVersion {
                 major: 0,
